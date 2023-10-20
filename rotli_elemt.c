96301fded3dc9b343b3ceadbae5ab96237d0607e
Mery9yuka <benhaida.maryem.com>
2023-10-20 02:16:52 +0100

    betty

--- rotli_elemt.c
+++ rotli_elemt.c
@@ -6,24 +6,24 @@
  */
 void rotli_elemt(stack_t **stack, unsigned int line_number)
 {
-    stack_t *top, *middle, *bottom;
-    (void)line_number;
+	stack_t *top, *middle, *bottom;
+	(void)line_number;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-        return;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 
-    top = *stack;
-    middle = top->next;
-    bottom = *stack;
+	top = *stack;
+	middle = top->next;
+	bottom = *stack;
 
-    while (bottom->next != NULL)
-    {
-        bottom = bottom->next;
-    }
+	while (bottom->next != NULL)
+	{
+		bottom = bottom->next;
+	}
 
-    bottom->next = top;
-    top->prev = bottom;
-    top->next = NULL;
-    middle->prev = NULL;
-    *stack = middle;
+	bottom->next = top;
+	top->prev = bottom;
+	top->next = NULL;
+	middle->prev = NULL;
+	*stack = middle;
 }
